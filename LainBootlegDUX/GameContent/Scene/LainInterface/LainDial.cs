522e7b1b7439b3c4b45deeeb19cb07e86bf1f775
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-04-17 17:42:23 +0400

    LainDial Update

---
+++ LainBootlegDUX/GameContent/Scene/LainInterface/LainDial.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace LainBootlegDUX.GameContent
+{
+    public class LainDial : GameEntity
+    {
+        Texture2D texture;
+
+        private Vector2Int dialDefaultWindowSize = new Vector2Int(200, 128);
+        private Vector2Int dialMinOffset = new Vector2Int(-36, -36);
+
+        public LainDial(GameScene scene) : base(scene)
+        {
+        }
+
+        public override void OnInitialize()
+        {
+            parent.fixedAspectRatio = false;
+        }
+
+        public override void OnLoadContent()
+        {
+            texture = graphicDevice.LoadTexture2D("Asset/lainSprite/upscaledBootlegSprites/460.png");
+
+            parent.Window.SetWindowSize(dialDefaultWindowSize);
+            parent.SetAspectRatioSize(dialDefaultWindowSize);
+        }
+
+        public override void OnUpdate(GameTime gameTime)
+        {
+
+        }
+
+        public override void OnDraw(GameTime gameTime)
+        {
+            Rectangle rectangle = graphicDevice.PresentationParameters.Bounds;
+            int scaledXOffset = (int) MathU.MapClampRanged(rectangle.Width, 0, dialDefaultWindowSize.x, 0, dialMinOffset.x);
+            int scaledYOffset = (int) MathU.MapClampRanged(rectangle.Height, 0, dialDefaultWindowSize.y, 0, dialMinOffset.y);
+            rectangle.Location = new Point(scaledXOffset, scaledYOffset);
+            spriteBth.Draw(texture, rectangle, Color.White);
+        }
+    }
+}
--- LainBootlegDUX/GameContent/Scene/LainInterface/LainInterfaceScene.cs
+++ LainBootlegDUX/GameContent/Scene/LainInterface/LainInterfaceScene.cs
@@ -6,23 +6,19 @@ namespace LainBootlegDUX.GameContent
 {
     public class LainInterfaceScene : GameScene
     {
-        Texture2D texture;
-
         public LainInterfaceScene()
         {
-           
+            sceneEntities.Add(new LainDial(this));
         }
 
         public override void OnInitialize()
         {
-            fixedAspectRatio = true;
+
         }
 
         public override void OnLoadContent()
         {
-            texture = GraphicsDevice.LoadTexture2D("Asset/lainSprite/upscaledBootlegSprites/460.png");
-            Window.SetWindowSize(new Vector2(200, 128));
-            SetAspectRatioSize(new Vector2(200, 128));
+
         }
 
         public override void OnUpdate(GameTime gameTime)
@@ -32,9 +28,7 @@ namespace LainBootlegDUX.GameContent
 
         public override void OnDraw(GameTime gameTime)
         {
-            Rectangle rectangle = GraphicsDevice.PresentationParameters.Bounds;
-            rectangle.Location = new Point(0, 0);   
-            spriteBatch.Draw(texture, rectangle, Color.White);
+
         }
     }
 }
