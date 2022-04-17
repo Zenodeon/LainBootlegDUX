177876fd0f4398d6d47ad631aa906268b74a07c8
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-04-17 17:40:00 +0400

    Math Utility Class

---
+++ LainBootlegDUX/GameContent/Utility/MathU.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LainBootlegDUX.GameContent
+{
+    public static class MathU
+    {
+        public static float MapClampRanged(float value, float InMinimum, float InMaximum, float OutMinimum, float OutMaximum)
+        {
+            var InRange = InMaximum - InMinimum;
+            var OutRange = OutMaximum - OutMinimum;
+            return ((value - InMinimum) * OutRange / InRange) + OutMinimum;
+        }
+    }
+}
