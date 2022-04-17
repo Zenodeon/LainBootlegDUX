514f78308afc89241205a94ef392c442a6258db4
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-04-17 17:40:25 +0400

    Vector2Int Class

---
+++ FNA/src/Vector2Int.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Xna.Framework
+{
+	public struct Vector2Int
+	{
+		public static Vector2Int zero = new Vector2Int(0, 0);
+		public static Vector2Int one = new Vector2Int(1, 1);
+
+		public Vector2 vector;
+
+		public int x
+		{
+			get => (int)vector.x;
+			set => vector.X = value;
+		}
+
+		public int y
+		{
+			get => (int)vector.y;
+			set => vector.y = value;
+		}
+
+		public Vector2Int(float x, float y)
+		{
+			vector = new Vector2(x, y);
+		}
+
+		public Vector2Int(Vector2 vector2)
+		{
+			vector = vector2;
+		}
+
+		public static bool operator !=(Vector2Int vectorA, Vector2Int vectorB)
+			=> !(vectorA == vectorB);
+
+		public static bool operator ==(Vector2Int vectorA, Vector2Int vectorB)
+			=> vectorA.vector == vectorB.vector;
+	}
+}
