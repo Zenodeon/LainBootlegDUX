2ace01e44b88e96c8c2a0fc1d15b52c214dae8bc
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-05-01 20:06:32 +0400

    Renaming Texture2DUtility

--- LainBootlegDUX/GameContent/Utility/LoadUtility.cs
+++ LainBootlegDUX/GameContent/Utility/LoadUtility.cs
@@ -1,14 +1,21 @@
 ﻿using System.IO;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
 
 namespace LainBootlegDUX.GameContent
 {
-    public static class Texture2DUtility
+    public static class LoadUtility
     {
         public static Texture2D LoadTexture2D(this GraphicsDevice graphicsDevice, string assetPath)
         {
             FileStream fileStream = new FileStream(assetPath, FileMode.Open, FileAccess.Read);
             return Texture2D.FromStream(graphicsDevice, fileStream);
+        }
+
+        public static SoundEffect LoadAudio(string assetPath)
+        {
+            FileStream fileStream = new FileStream(assetPath, FileMode.Open, FileAccess.Read);
+            return SoundEffect.FromStream(fileStream);
         }
     }
 }
